7439828392343ad8e36d28c3c6b36524de31d459
DotCipher <71338128+Wiran-Larbi@users.noreply.github.com>
2024-06-04 17:02:36 +0100

    Update environment.prod.ts

--- frontend/src/app/core/environments/environment.prod.ts
+++ frontend/src/app/core/environments/environment.prod.ts
@@ -1,8 +1,5 @@
 export const environment = {
   production: true,
   rootUrl: 'http://localhost:3008',
-  apiBaseUrl: 'http://localhost:3008/api/v1',
-  loanTypes: ['Personal Loan','Home Loan','Car Loan','Education Loan','Gold Loan'],
-  cardTypes: ['Credit Card','Debit Card'],
-  transactionTypes: ['Credit','Debit']
+  apiBaseUrl: 'http://localhost:3008/api/v1'
 }
